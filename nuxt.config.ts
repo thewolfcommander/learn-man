7d70c7423c502185c4d3581e80114776bb094a91
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2024-01-16 17:39:51 +0530

    chore: firebase and primevue setup dnoe

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  modules: [
+      'nuxt-primevue'
+  ],
+  devtools: { enabled: true },
+  // primevue: {
+  //     /* Options */
+  // },
+  css: ['primevue/resources/themes/lara-light-green/theme.css'],
 })
